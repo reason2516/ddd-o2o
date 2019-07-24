ce3b6dbe4d8050c1bfa741c3785e772de03bdcf2
wangmingxu <wangmingxu@bcpeople.com>
2019-07-24 19:35:39 +0800

    repush

--- api-customer/src/main/java/com/wmx/o2o/api/customer/ApiCustomerApplication.java
+++ api-customer/src/main/java/com/wmx/o2o/api/customer/ApiCustomerApplication.java
@@ -3,9 +3,7 @@ package com.wmx.o2o.api.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication
--- serv-identity/src/main/java/com/wmx/o2o/serv/identity/resource/controller/UserController.java
+++ serv-identity/src/main/java/com/wmx/o2o/serv/identity/resource/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/user")
@@ -27,7 +28,9 @@ public class UserController {
     private Environment environment;
 
     @RequestMapping(value = "/get")
-    public UserDO get(@RequestParam Long id) {
+    public UserDO get(@RequestParam Long id) throws InterruptedException {
+//        int i = new Random().nextInt(10000);
+//        Thread.sleep(i);
         UserDO userDO = this.userRepositoryJpa.findById(id).orElse(null);
         userDO.setUsername(userDO.getUsername() + environment.getProperty("server.port") + "-" + environment.getProperty("spring.profiles.active"));
         return userDO;
